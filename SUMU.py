1d5736d5fd6c6328d35e0e54ce1c2c11a75fa074
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-11 15:37:28 +0600

    Update SUMU.py

--- SUMU.py
+++ SUMU.py
@@ -135,7 +135,7 @@ def back():
 #LOGO
 def banner():
 	clear()
-        print("""%s
+       print("""
 banner = """
   \033[0;93m ██████  ██  ██████  ██████ ██   ██ ██ 
   \033[0;93m ██   ██ ██ ██      ██      ██   ██ ██ 

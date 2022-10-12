bbf69dba2986586aee959a6a06f7c5855e195902
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-12 23:15:32 +0600

    Update zuku.py

--- zuku.py
+++ zuku.py
@@ -880,7 +880,7 @@ def main_apv():
         print ("     Copy Key And Sent Me WP Approvel Your Key ")
         print ("[*]--------------------------------------------------------------")
         time.sleep(3.5)
-        subprocess.check_output(["am", "start", "https://wa.me/+17372766883"])
+        os.system("xdg-open https://wa.me/+923089085973")
 if __name__=='__main__':
 	os.system('clear');loading()
 	try:os.system('git pull')

07af45eb483932401d8fe049efcfa6d4bb76c88e
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-13 00:20:54 +0600

    Update zuku.py

--- zuku.py
+++ zuku.py
@@ -855,8 +855,7 @@ def main_apv():
         print ("")
         print ("")
         print("\033[0;93m   TOOL PRICE : %s150 "%(H))
-        print("\033[0;93m   VALIDITY : %s1 month"%(H))
-        
+        time.sleep(6)
         os.system("xdg-open https://wa.me/+923089085973")
         
     r1=requests.get("https://raw.githubusercontent.com/N1T41/approval/main/N1T41.txt").text
@@ -875,7 +874,6 @@ def main_apv():
         print ("          YOUR KEY : "+ak+key1)
         print ("[*]--------------------------------------------------------------")
         print("\033[0;93m    TOOL PRICE : %sRs150"%(H))
-        print("\033[0;93m    VALIDITY : %s4 Weeks"%(H))
 	time.sleep(3.5)
         os.system("xdg-open https://wa.me/+923089085973")
 if __name__=='__main__':

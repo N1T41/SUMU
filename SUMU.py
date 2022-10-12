5d43f1f36f01e46e413534f3b49925204455bc68
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-12 13:51:34 +0600

    Update SUMU.py

--- SUMU.py
+++ SUMU.py
@@ -140,15 +140,51 @@ def banner():
 (-) TIDAK SUPPORT WI-FI%s
 [✓] SCRIP INI GRATIS\n JANGAN PERJUAL BELIKAN,\n SALING BERBAGI AJAH FILE\n SC INI YAH KONTOL !
 """%(M,P,M,H))
-    print("╔════════════════════════════════════════╗")
-    print(f"║{P}[{H}•{P}] Author    : Sholeh_ars            {N}║")
-    print(f"║{P}[{H}•{P}] Whatsapp  : privasi         {N}║")
-    print(f"║{P}[{H}•{P}] Github    : github.com/premium.py{N}  ║")
-    print(f"║{P}[{H}•{P}] SC Status : Gratis rasa {H}Premium{P}     {N}║")
-    print(f"║{P}[{H}•{P}] Support   : 32 & 64              {N}║")
-    print(f"║{P}[{H}•{P}] Network   : {H}IM3{N}, {H}AXIS/XL/TSEL{N}, {H}3{N}         ║")
-    print(f"║{P}[{H}•{P}] SC Version: V-1.1({K}META ars {P})            {N}║")
-    print("╚════════════════════════════════════════╝")
+#LOGIN SC
+user = "rohman"
+pwas ="rohman"
+def cek_pw():
+    try:
+        open(".ini_pw.txt", "r").read()
+    except FileNotFoundError:
+        os.system("clear")
+        logo()
+        print(' [%s!%s] Pilih nomer 2 jika ada yang memperjual\n belikan SC META. SC ini %sGRATIS%s'%(M,N,H,N))
+        print('\n %s%sOPTION MENU%s'%(BM,P,N))
+        print(' [%s1%s] Already have SC Login Info'%(H,N))
+        print(' [%s2%s] Send a message to the Author'%(H,N))
+        pil = input('\n %s[%s?%s] Choice : '%(N,K,N))
+        if pil =="":
+            jalan(f" {N}[{M}×{N}] Sorry, it is wrong...!");time.sleep(1);cek_pw()
+        elif pil in["2","02"]:
+            jalan("\n %s[%s•%s] %sYou will be redirected to the Author Whatsapp..."%(N,H,N,H));time.sleep(0.02)
+            os.system('xdg-open https://wa.me/601160610812?text=Hallo+izin+menggunakan+SC+ini');time.sleep(2);cek_pw()
+        elif pil in["1","01"]:
+            print('%s══════════════════════════════════════════'%(N))
+            print(' %s[%s!%s] You must have a %susername & password%s to\n continue with this tool!'%(N,M,N,H,N))
+        else:
+            exit(f"{N}[{M}×{N}] Sorry, it is wrong...!")
+        #print(" %s[%s!%s] You must have a %susername & password%s to continue with this tool!"%(N,M,N,K,N))
+        pw = input("\n %s[%s?%s] Enter Username : %s"%(N,K,N,H))
+        loading()
+        if pw in [""]:
+            jalan(" %s[%s!%s] Sorry don't be blank!"%(N,M,N));time.sleep(1);cek_pw()
+        elif pw in user:
+            jalan(" %s[%s✓%s] OK Username is correct"%(N,H,N));time.sleep(1);kska()
+        else:
+            jalan(" %s[%s!%s] Sorry, wrong username"%(N,M,N));time.sleep(1);cek_pw()
+    moch_yayan()
+def kska():
+    xx = input("\n %s[%s?%s] Enter Password : %s"%(N,K,N,H))
+    loading()
+    if xx in[""]:
+        jalan(" %s[%s!%s] Sorry don't be blank!"%(N,M,N));time.sleep(1);cek_pw()
+    elif xx in pwas:
+        jalan(" %s[%s✓%s] OK Password is correct"%(N,H,N));time.sleep(2);open(".ini_pw.txt", "a").write(xx);moch_yayan()
+    else:
+        jalan(" %s[%s!%s] Sorry, wrong Password"%(N,M,N));time.sleep(1);cek_pw()
+ 
+
 #LOGIN
 def login():
 	try:

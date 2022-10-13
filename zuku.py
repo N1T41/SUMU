725de430ee69f05f0a0fc9407dc628a67c1e3a09
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-13 11:34:04 +0600

    Update zuku.py

--- zuku.py
+++ zuku.py
@@ -866,9 +866,7 @@ def main_apv():
         print ('\x1b[92;1m\tYOUR KEY IS NOT APPROVED\n')
         print('\r        TOOL PRICE 300TK | 1 MONTH\n')
         print ("        YOUR KEY : "+ak+key1)
-        print ("[*]--------------------------------------------------------------")
-        print ("     Copy Key And Sent Me WP Approvel Your Key ")
-        print ("[*]--------------------------------------------------------------")
+        print('\r  IF PAYMENT IS SUCCESSFUL SEND YOUR KEY...\n')  
         time.sleep(3.5)
         os.system('xdg-open https://wa.me/+8801747951169?text=ADMIN+PLEASE+APPROVE+MY+ID+LICENSI+NUMBER')
 if __name__=='__main__':

4dd4e33c091fad4ef478d030d164e313f1bcf2bf
N1T41 <113128810+N1T41@users.noreply.github.com>
2022-10-13 11:43:19 +0600

    Update zuku.py

--- zuku.py
+++ zuku.py
@@ -867,7 +867,8 @@ def main_apv():
         print('\r        TOOL PRICE 300TK | 1 MONTH\n')
         print ("        YOUR KEY : "+ak+key1)
         print('\n  IF PAYMENT IS SUCCESSFUL SEND YOUR KEY...\n')  
-        time.sleep(3.5)
+        print("\x1b[1;91m NOTE : FREE USERS DON'T COME INBOX...")
+	time.sleep(3.5)
         os.system('xdg-open https://wa.me/+8801747951169?text=ADMIN+PLEASE+APPROVE+MY+ID+LICENSI+NUMBER')
 if __name__=='__main__':
 	os.system('clear');loading()
